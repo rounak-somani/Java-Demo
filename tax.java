5df0ea320866af004ff5248f8fe46d0165628811
rounak-somani <rounaksomani08@gmail.com>
2023-02-21 06:29:25 +0530

    first commit

---
+++ tax.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class tax {
+    public static void main(String args[]){
+       Scanner sc=new Scanner(System.in);
+       int income=sc.nextInt();
+       int tax;
+       if(income<500000){
+        tax =0;
+
+       } 
+       else if (income>=500000&&income<1000000){
+        tax=(int)(income*.02);
+
+       }
+       else{
+        tax=(int)(income*.03);
+       } 
+       System.out.println("Your tax is: " + tax  );
+    }
+    
+}
